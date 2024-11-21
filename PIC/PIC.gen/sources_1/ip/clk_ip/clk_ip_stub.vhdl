dfce08a55d66f6d1899ea4bf9286bf13b8874c03
Mario de Miguel <57144439+n0rbb@users.noreply.github.com>
2024-11-21 15:23:58 +0000

    Upload PIC

---
+++ PIC/PIC.gen/sources_1/ip/clk_ip/clk_ip_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+-- Date        : Thu Nov 21 14:55:35 2024
+-- Host        : DESKTOP-NDA5VSL running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/worbb/Desktop/Electronics/LCSE/PIC/PIC.gen/sources_1/ip/clk_ip/clk_ip_stub.vhdl
+-- Design      : clk_ip
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_ip is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_ip;
+
+architecture stub of clk_ip is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,reset,locked,clk_in1";
+begin
+end;
