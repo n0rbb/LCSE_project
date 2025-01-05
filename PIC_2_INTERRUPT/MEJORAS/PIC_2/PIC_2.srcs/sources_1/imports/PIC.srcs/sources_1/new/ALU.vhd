da352c7f09021794a085c7b0871d0c07f280c96c
Mario de Miguel <57144439+n0rbb@users.noreply.github.com>
2025-01-06 00:45:22 +0100

    Elimino carpeta PIC 1 y subo todo ya en "orden"
    RIP el PIC 1, que nunca llegó a funcionar. Y nunca sabremos por qué. Seguramente por el mismo error que tenía el PIC 2 en la RAM y que tanto me costó depurar (32 días, nada más y nada menos)

--- PIC_2_INTERRUPT/MEJORAS/PIC_2/PIC_2.srcs/sources_1/imports/PIC.srcs/sources_1/new/ALU.vhd
+++ PIC_2_INTERRUPT/MEJORAS/PIC_2/PIC_2.srcs/sources_1/imports/PIC.srcs/sources_1/new/ALU.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.11.2024 15:51:46
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
@@ -25,133 +7,156 @@ use work.PIC_pkg.all;
 
 entity ALU is
   port (
-    Reset         : in std_logic; -- asynnchronous, active low 
-    Clk           : in std_logic; -- Sys clock, 20MHz, rising_edge 
-    ALU_op        : in alu_op; -- u-instructions from CPU 
-    FlagZ         : out std_logic; -- Zero  
-    FlagC         : out std_logic; -- Carry  
-    FlagN         : out std_logic; -- Nibble 
-    FlagE         : out std_logic; -- Error  
-    Index_Reg     : out std_logic_vector(7 downto 0); -- Index register 
-    Databus       : inout std_logic_vector(7 downto 0) -- System Data bus
+    Reset         : in std_logic; 
+    Clk           : in std_logic; 
+    ALU_op        : in alu_op; -- u-instructions 
+    FlagZ         : out std_logic; 
+    FlagC         : out std_logic; 
+    FlagN         : out std_logic; 
+    FlagE         : out std_logic; 
+    Index_Reg     : out std_logic_vector(7 downto 0); 
+    Databus       : inout std_logic_vector(7 downto 0) 
   );
 end ALU;
 
 architecture Behavioral of ALU is
-  signal A_next, B_next, ACC_next, Index_Reg_next       : std_logic_vector(7 downto 0);
-  signal FlagZ_next, FlagC_next, FlagN_next, FlagE_next : std_logic;
-  signal FlagZ_i, FlagC_i, FlagN_i, FlagE_i : std_logic;
+
+  --Registro para guardas temporales
+  signal A_temp, B_temp, ACC_temp, Index_Reg_temp       : std_logic_vector(7 downto 0);
+  signal FlagZ_temp, FlagC_temp, FlagN_temp, FlagE_temp : std_logic;
+  signal FlagZ_i, FlagC_i, FlagN_i, FlagE_i             : std_logic;
+  --Señales de salida
   signal A, B, ACC, Index_Reg_i                         : std_logic_vector(7 downto 0);
-  signal Sum       : std_logic_vector(8 downto 0);
-  signal Sum4      : std_logic_vector(4 downto 0);
+  --Resultados
+  signal Sum                                            : std_logic_vector(8 downto 0);
+  signal Sum4bits                                       : std_logic_vector(4 downto 0);
+  --Constante de conversión ASCII a binario
   constant ASCII0 : unsigned := to_unsigned(48, 6);
-  
   -- Registros para guardar y recuperar el contexto
   signal A_r, B_r, ACC_r, Index_Reg_r : std_logic_vector(7 downto 0);
   signal FlagZ_r, FlagC_r, FlagN_r, FlagE_r : std_logic;
---  DON'T TOUCH
---  attribute dont_touch : string;
---  attribute dont_touch of ALU_op : signal is "true";
---  attribute dont_touch of ACC : signal is "true";
---  attribute dont_touch of FlagC_i : signal is "true";
---  attribute dont_touch of FlagN_i : signal is "true";
---  attribute dont_touch of FlagE_i : signal is "true";
+
 begin
-  combinacional : process (ALU_op, A, B, ACC, Index_Reg_i, Sum, Sum4, ACC_next, Databus, FlagZ_i, FlagC_i, FlagN_i, FlagE_i)
+
+  combinacional : process (ALU_op, A, B, ACC, Index_Reg_i, Sum, Sum4bits, ACC_temp, Databus, FlagZ_i, FlagC_i, FlagN_i, FlagE_i)
   begin
-    --Default
-    A_next         <= A;
-    B_next         <= B;
-    ACC_next       <= ACC;
-    Index_Reg_next <= Index_Reg_i;
-    FlagZ_next     <= FlagZ_i;
-    FlagC_next     <= FlagC_i;
-    FlagN_next     <= FlagN_i;
-    FlagE_next     <= FlagE_i;
+    --Valores por defecto
+    A_temp         <= A;
+    B_temp         <= B;
+    ACC_temp       <= ACC;
+    Index_Reg_temp <= Index_Reg_i;
+    FlagZ_temp     <= FlagZ_i;
+    FlagC_temp     <= FlagC_i;
+    FlagN_temp     <= FlagN_i;
+    FlagE_temp     <= FlagE_i;
     Sum <= (others => '0');
-    Sum4 <= (others => '0');
+    Sum4bits <= (others => '0');
     Databus <= (others => 'Z');
+    
     case ALU_op is
       when nop =>
+      
       when op_lda =>
-        A_next <= Databus;
+        A_temp <= Databus;
+        
       when op_ldb =>
-        B_next <= Databus;
+        B_temp <= Databus;
+        
       when op_ldacc =>
-        ACC_next <= Databus;
+        ACC_temp <= Databus;
+        
       when op_ldid =>
-        Index_Reg_next <= Databus;
+        Index_Reg_temp <= Databus;
+        
       when op_mvacc2id =>
-        Index_Reg_next <= ACC;
+        Index_Reg_temp <= ACC;
+        
       when op_mvacc2a =>
-        A_next <= ACC;
+        A_temp <= ACC;
+        
       when op_mvacc2b =>
-        B_next <= ACC;
+        B_temp <= ACC;
+        
       when op_add =>
-        Sum4 <= std_logic_vector(unsigned('0'&A(3 downto 0)) + unsigned('0'&B(3 downto 0)));
+        --La suma se hace por separado para ver si hay carry en el primer byte.
+        Sum4bits <= std_logic_vector(unsigned('0'&A(3 downto 0)) + unsigned('0'&B(3 downto 0)));
         Sum <= std_logic_vector(unsigned('0'&A) + unsigned('0'&B));
-        ACC_next <= Sum(7 downto 0);
-        FlagC_next <= Sum(8);
-        FlagN_next <= Sum4(4);
+        ACC_temp <= Sum(7 downto 0);
+        FlagC_temp <= Sum(8);
+        FlagN_temp <= Sum4bits(4);
         if unsigned(Sum) = 0 then
-            flagZ_next <= '1';
+            flagZ_temp <= '1';
         end if;
+        
       when op_sub =>
-        Sum4 <= std_logic_vector(unsigned('0'&A(3 downto 0)) - unsigned('0'&B(3 downto 0)));
+        Sum4bits <= std_logic_vector(unsigned('0'&A(3 downto 0)) - unsigned('0'&B(3 downto 0)));
         Sum <= std_logic_vector(unsigned('0'&A) - unsigned('0'&B));
-        ACC_next <= Sum(7 downto 0);
-        FlagC_next <= Sum(8);
-        FlagN_next <= Sum4(4);
+        ACC_temp <= Sum(7 downto 0);
+        FlagC_temp <= Sum(8);
+        FlagN_temp <= Sum4bits(4);
         if unsigned(Sum) = 0 then
-            flagZ_next <= '1';
+            flagZ_temp <= '1';
         end if;
+        
         when op_shiftl =>
-            ACC_next <= ACC(6 downto 0) & '0';
+            ACC_temp <= ACC(6 downto 0) & '0';
+            
         when op_shiftr =>
-            ACC_next <= '0' & ACC(7 downto 1);
+            ACC_temp <= '0' & ACC(7 downto 1);
+            
         when op_and =>
-            ACC_next <= A and B;
+            ACC_temp <= A and B;
+            
         when op_or =>
-            ACC_next <= A or B;
+            ACC_temp <= A or B;
+            
         when op_xor =>
-            ACC_next <= A xor B;
+            ACC_temp <= A xor B;
+            
         when op_cmpe =>
             if unsigned(A) = unsigned(B) then
-                flagZ_next <= '1';
+                flagZ_temp <= '1';
             else
-                flagZ_next <= '0';
+                flagZ_temp <= '0';
             end if;
+            
         when op_cmpl =>
             if unsigned(A) < unsigned(B) then
-                flagZ_next <= '1';
+                flagZ_temp <= '1';
             else
-                flagZ_next <= '0';
+                flagZ_temp <= '0';
             end if;
+            
         when op_cmpg =>
             if unsigned(A) > unsigned(B) then
-                flagZ_next <= '1';
+                flagZ_temp <= '1';
             else
-                flagZ_next <= '0';
+                flagZ_temp <= '0';
             end if;
+            
         when op_ascii2bin =>
-            ACC_next <= std_logic_vector(unsigned(A) - ASCII0);
-            if(unsigned(ACC_next) > 9) then
-                flagE_next <= '1';
+            ACC_temp <= std_logic_vector(unsigned(A) - ASCII0);
+            if(unsigned(ACC_temp) > 9) then
+                flagE_temp <= '1';
             else
-                flagE_next <= '0';
+                flagE_temp <= '0';
             end if;
+            
         when op_bin2ascii =>
-            ACC_next <= std_logic_vector(unsigned(A) + ASCII0);
-            if((unsigned(ACC_next) > (ASCII0+9)) or (unsigned(ACC_next) < ASCII0)) then
-                flagE_next <= '1';
+            ACC_temp <= std_logic_vector(unsigned(A) + ASCII0);
+            if((unsigned(ACC_temp) > (ASCII0+9)) or (unsigned(ACC_temp) < ASCII0)) then
+                flagE_temp <= '1';
             else
-                flagE_next <= '0';
+                flagE_temp <= '0';
             end if;
+            
         when op_oeacc =>
             Databus <= ACC;
             
+        --Operaciones añadidas para el uso de interrupciones
         when op_savecontext =>
         when op_loadcontext =>
+        
     end case;
   end process combinacional;
   
@@ -191,14 +196,14 @@ begin
         FlagE_i     <= FlagE_r;
         
       else
-        A           <= A_next;
-        B           <= B_next;
-        ACC         <= ACC_next;
-        Index_Reg_i <= Index_Reg_next;
-        FlagZ_i     <= FlagZ_next;
-        FlagC_i     <= FlagC_next;
-        FlagN_i     <= FlagN_next;
-        FlagE_i     <= FlagE_next;
+        A           <= A_temp;
+        B           <= B_temp;
+        ACC         <= ACC_temp;
+        Index_Reg_i <= Index_Reg_temp;
+        FlagZ_i     <= FlagZ_temp;
+        FlagC_i     <= FlagC_temp;
+        FlagN_i     <= FlagN_temp;
+        FlagE_i     <= FlagE_temp;
       end if;
       
       if ALU_op = op_savecontext then
@@ -211,17 +216,15 @@ begin
         FlagN_r <= FlagN_i;
         FlagE_r <= FlagE_i;
       end if;
-      
     end if;
   end process secuencial;
   
-  
+  --Pasamos las señales intermedias a las salidas del bloque.
   Index_Reg <= Index_Reg_i;
   FlagZ     <= FlagZ_i;
   FlagC     <= FlagC_i;
   FlagN     <= FlagN_i;
-  FlagE     <= FlagE_i;
-  
+  FlagE     <= FlagE_i; 
   
 end Behavioral;
 
